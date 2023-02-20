b63113a20a2e5be713b19ef623b58f23318b5e80
Bruno Cipriano <cipriano990@gmail.com>
2023-02-20 08:03:58 -0300

    conclusão do front end

--- backend/minhasfinancas/src/main/java/com/bcipriano/minhasfinancas/MinhasfinancasApplication.java
+++ backend/minhasfinancas/src/main/java/com/bcipriano/minhasfinancas/MinhasfinancasApplication.java
@@ -2,10 +2,23 @@ package com.bcipriano.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MinhasfinancasApplication {
+@EnableWebMvc
+public class MinhasfinancasApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.maxAge(3600);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
 	}
